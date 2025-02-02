f390ea9b7fa3dd910b9031309571345310415706
dzungvd98 <yungvd98@gmail.com>
2025-02-02 21:08:53 +0700

    Update solution for 122

--- BestTimeToBuyAndSellStockII_122.java
+++ BestTimeToBuyAndSellStockII_122.java
@@ -1,18 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-
-/**
- *
- * @author duydu
- */
-public class BestTimeToBuyAndSellStockII_122 {
-
-    /**
-     * @param args the command line arguments
-     */
+class Solution {
+    public static int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int size = prices.length;
+        int sumProfit = 0;
+        for(int i = 0; i  < size - 1; i++) {
+            if(prices[i] > prices[i+1]) {
+                sumProfit += prices[i] - buy;
+                buy = prices[i+1];
+            }
+        }
+        sumProfit += prices[size - 1] - buy;
+        return sumProfit;
+    }
+    
     public static void main(String args[]) {
-        // TODO code application logic here
+        int[] nums = {7,1,5,3,6,4};
+        System.out.println(maxProfit(nums));
     }
 }
