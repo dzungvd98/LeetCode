0103ea8f382a58c13bc5227e26d3ae31122d59a3
dzungvd98 <yungvd98@gmail.com>
2025-02-16 21:58:57 +0700

    Updated solution 28

--- FindTheIndexOfTheFirstOccurrenceInAString_28.java
+++ FindTheIndexOfTheFirstOccurrenceInAString_28.java
@@ -1,24 +1,35 @@
 class Solution {
     public static int strStr(String haystack, String needle) {
-        int i = 0, j, count;
-        while( i < haystack.length()) {
-            if(haystack.charAt(i) == needle.charAt(0)) {
-                j = 0;
-                
-                int k = i;
-                count = 0;
-                while(k < haystack.length() && j < needle.length()) {
-                    if(haystack.charAt(k) == needle.charAt(j)) {
-                        count++;
-                        k++;
-                        j++;
-                    } else break;
-                }
-                if(count == needle.length()) return i;
-            }
-            i++;
-        }
-        return -1;
+//        int i = 0, j, count;
+//        while( i < haystack.length()) {
+//            if(haystack.charAt(i) == needle.charAt(0)) {
+//                j = 0;
+//                
+//                int k = i;
+//                count = 0;
+//                while(k < haystack.length() && j < needle.length()) {
+//                    if(haystack.charAt(k) == needle.charAt(j)) {
+//                        count++;
+//                        k++;
+//                        j++;
+//                    } else break;
+//                }
+//                if(count == needle.length()) return i;
+//            }
+//            i++;
+//        }
+//        return -1;
+
+          int sl = needle.length();
+          if(haystack.equals(needle)) {
+              return 0;
+          }
+          for(int i = 0; i <= haystack.length() - sl; i++) {
+              if(haystack.substring(i, i + sl).equals(needle)) {
+                  return i;
+              }
+          }
+          return -1;
     }
     
     public static void main(String[] args) {
