612bf98495e4920528677639fbdfe13e1f04c7d8
dzungvd98 <yungvd98@gmail.com>
2025-02-18 21:13:42 +0700

    Updated solution 392

---
+++ IsSubsequence_392.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static boolean isSubsequence(String s, String t) {
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        
+        if(charS.length == 0) return false;
+        int i = 0;
+        int j = 0;
+        while(j < charT.length) {
+            if(charS[i] == charT[j]) {
+                i++;
+                j++;
+                if(i >= charS.length) return true;
+            } else {
+                j++;
+            } 
+        }
+        return false;   
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("", "ahbgdc"));
+    }
+}
