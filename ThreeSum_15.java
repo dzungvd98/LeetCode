b89c04da3fa08b2bc8f1f1674cfd10066e83e738
dzungvd98 <yungvd98@gmail.com>
2025-02-25 22:26:28 +0700

    updated solution 15

---
+++ ThreeSum_15.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2 && nums[i] <= 0; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            sol(i + 1, -nums[i], nums, res);
+        }
+
+        return res;
+    }
+
+    public void sol(int start, int target, int[] nums, List<List<Integer>> res){
+        int end = nums.length-1;
+
+        while(start < end){
+            if(nums[start] + nums[end] > target){
+                end--;
+                continue;
+            }
+
+            if(nums[start] + nums[end] < target){
+                start++;
+                continue;
+            }
+
+            res.add(Arrays.asList(-target, nums[start], nums[end]));
+            start++;
+            end--;
+
+            while(start <= end && nums[start] == nums[start-1])
+                start++;
+        }
+    }
+}
