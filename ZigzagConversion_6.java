3f29db1d45a37813e7ea556e86bdd1cf0ca14de3
dzungvd98 <yungvd98@gmail.com>
2025-02-28 23:04:45 +0700

    Updated solution 6

---
+++ ZigzagConversion_6.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        int length = s.length();
+
+        char[] result = new char[length];
+        int result_index = 0;
+        final int original_offset = numRows + (numRows - 2);
+        int offset = original_offset;
+
+        for (int line = 0; line < numRows && result_index < length; line++) {
+            result[result_index++] = s.charAt(line);
+
+            for (int index = line + offset; index < length && result_index < length;
+                 index += offset) {
+                result[result_index++] = s.charAt(index);
+
+                if (original_offset - offset > 0) {
+                    index += original_offset - offset;
+
+                    if (index < length) {
+                        result[result_index++] = s.charAt(index);
+                    }
+                }
+            }
+
+            offset -= 2;
+
+            if (offset == 0) {
+                offset = original_offset;
+            }
+        }
+
+        return new String(result);
+    }
+}
