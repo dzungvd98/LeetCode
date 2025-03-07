311f859dbaa00eb340d28b66b4db7a54d0aecfa5
dzungvd98 <yungvd98@gmail.com>
2025-03-07 22:43:50 +0700

    Updated solution 26 py

---
+++ RemoveElement_27.py
@@ -0,0 +1,30 @@
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+if __name__ == '__main__':
+    solu = Solution()
+    nums = [3,2,2,3]
+    print(solu.removeElement(nums, 3))
