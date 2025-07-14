0284f502711106fada7897e435085cd70c0e34c0
dzungvd98 <yungvd98@gmail.com>
2025-07-14 22:28:22 +0700

    update solution for substring without repeat

--- LongestSubstringWithoutRepeatingCharacters_3.py
+++ LongestSubstringWithoutRepeatingCharacters_3.py
@@ -13,6 +13,19 @@ class Solution(object):
 
         return max_length
 
+    def version2(self, s):
+        char_index = {}
+        start = 0
+        max_length = 0
+
+        for end, char in enumerate(s):
+            if char in char_index and char_index[char] >= start:
+                start = char_index[char] + 1
+            char_index[char] = end
+            max_length = max((max_length, end - start + 1))
+
+        return max_length
+
 
 if __name__ == "__main__":
     solution = Solution()
