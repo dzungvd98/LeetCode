5a7270f9406eadb4b98cf180e4377854169cf77a
dzungvd98 <yungvd98@gmail.com>
2025-07-21 22:33:57 +0700

    Update solution for 30

--- SubstringwithConcatenationofAllWords_30.java
+++ SubstringwithConcatenationofAllWords_30.java
@@ -1,18 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+import java.util.List;
+import java.util.ArrayList;
 
-/**
- *
- * @author PhaoPC
- */
 public class SubstringwithConcatenationofAllWords_30 {
+    
+    public static int findLengthOfSubstring(String[] words){
+        int result = 0;
+        for(String word : words) {
+            result += word.length();
+        }
+        
+        return result;
+    }
+    
 
-    /**
-     * @param args the command line arguments
-     */
+    public static List<Integer> findSubstring(String s, String[] words) {
+        System.out.println(findLengthOfSubstring(words));
+        return new ArrayList<Integer>();
+    }
+    
+    
     public static void main(String args[]) {
-        // TODO code application logic here
+        String words[] = {"word","good","best","words"};
+        
+        findSubstring("aaaa", words);
+        
     }
 }
